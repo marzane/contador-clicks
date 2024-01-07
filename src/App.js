3ae8e143b2f7ad5462df70dbb7ff2045564dce0b
marzane <marzaneporquesi@gmail.com>
2024-01-07 17:41:06 +0100

    commit final

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import './App.css';
 import logoFreeCodeCamp from "./img/freeCodeCamp.svg";
-import Boton from "./components/Boton.js";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import { useState } from 'react';
 
 function App() {
 
-  function manejarClick(){
-    console.log("click");
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
   }
 
-  function reiniciarContador(){
-    console.log("reiniciar");
+  const reiniciarContador = () => {
+    setNumClicks(0);
   }
 
   return (
     <div className="App">
       <div className='logo-freecodecamp-contenedor'>
-        <img 
+        <img
           className='logo-freecodecamp'
           src={logoFreeCodeCamp}
-          alt='logo-freecodecamp'/>
+          alt='logo-freecodecamp' />
       </div>
       <div className='contador-contenedor-principal'>
-
+        <Contador
+          numClicks={numClicks}
+        />
         <div className='contenedor-botones'>
-          <Boton 
+          <Boton
             texto="click"
             esBotonClick={true}
             manejarClick={manejarClick}
           />
-          <Boton 
+          <Boton
             texto="reset"
             esBotonClick={false}
             manejarClick={reiniciarContador}
---
+++ src/components/Contador.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Contador.css";
+
+function Contador({ numClicks }) {
+
+  return (
+    <div className="contador">
+      {numClicks}
+    </div>
+  );
+}
+
+export default Contador;
